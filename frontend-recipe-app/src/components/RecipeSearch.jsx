a897a941df6e6e3f9e06151cda4fe776ff652123
Travis Nelson <tjnel6691@gmail.com>
2022-10-26 13:01:09 -0400

    Heroku deployment

--- frontend-recipe-app/src/components/RecipeSearch.jsx
+++ frontend-recipe-app/src/components/RecipeSearch.jsx
@@ -3,7 +3,7 @@ const RecipeSearch = (props) => {
     <div>
       <h2>Recipe Search</h2>
       <form id="Search-form">
-        <label for="searchType">Search recipes by:</label>
+        <label for="searchType">Search Recipes By:</label>
         <select id="searchType" name="searchType">
           <option value="Name">Name</option>
           <option value="Ingredients">Ingredients</option>
