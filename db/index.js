8235040ca1d22740033858302958e68f26ecb76b
Travis Nelson <tjnel6691@gmail.com>
2022-11-01 15:29:11 -0400

    renamed name in client package.json

--- db/index.js
+++ db/index.js
@@ -6,14 +6,13 @@ let url = process.env.MONGODB_URI
 mongoose
   .connect(url)
   .then(() => {
-    console.log(
-      `Successfully connected to MongoDB via ${process.env.MONGODB_URI}.`
-    )
+    console.log(`Successfully connected to MongoDB via ${url}.`)
   })
   .catch((e) => {
     console.error('Connection error', e.message)
   })
 mongoose.set('debug', true)
+
 const db = mongoose.connection
 
 module.exports = db
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express')
-const cors = require('cors')
-const logger = require('morgan')
-const PORT = process.env.PORT || 3001
-const db = require('./db')
 const routes = require('./routes')
+const db = require('./db')
+const logger = require('morgan')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 3001
+
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+
 app.use(logger('dev'))
+
+app.use(cors())
+
 app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/api', routes)
