86c555771f79960096c9db790820f6a2f029ab72
Travis Nelson <tjnel6691@gmail.com>
2022-11-05 13:05:26 -0400

    Added text to About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,8 +3,22 @@ const About = () => {
     <div className="about">
       <h2>About</h2>
       <p>Thanks for checking out my website!</p>
-      <p></p>
-      <p></p>
+      <p>
+        This is my first full stack application. That means I had to build both
+        the website you are looking at, (aka the "front-end") as well as the
+        server and database (aka "back-end") it needs to communicate with to
+        create, read, update, and delete the data that is displayed on the
+        front-end as you use the app.
+      </p>
+      <p>
+        Most of the functionality of this app was built during a week-long
+        sprint for my second General Assembly coding project. I then spent
+        another week polishing the appearance in my spare time, resulting in the
+        website you are now using. I plan to continue updating the site and will
+        eventually include it as part of my portfolio. I hope you find it
+        useful, or at least interesting to look at!
+      </p>
+      <h4>- Travis Nelson</h4>
     </div>
   )
 }
