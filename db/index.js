dbf82b144b3b6997fdc3b56bef44800b947b245c
Travis Nelson <tjnel6691@gmail.com>
2023-04-13 18:08:00 -0400

    Changed local db string and build script

--- db/index.js
+++ db/index.js
@@ -4,12 +4,12 @@ require('dotenv').config()
 const MONGODB =
   process.env.NODE_ENV === 'production'
     ? process.env.MONGODB_URI
-    : process.env.DEVELOPMENT_URI
+    : 'mongodb://localhost:27017'
 
 mongoose
   .connect(MONGODB)
   .then(() => {
-    console.log(`Successfully connected to MongoDB via ${url}.`)
+    console.log(`Successfully connected to MongoDB via ${MONGODB}.`)
   })
   .catch((e) => {
     console.error('Connection error', e.message)
